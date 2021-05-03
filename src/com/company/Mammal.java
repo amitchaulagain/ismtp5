8743fe28c7e079fd14ddb34405322073b0e6198b
amitchaulagain <achaulagain123@gmail.com>
2021-05-03 08:27:25 +0545

    first commit

---
+++ src/com/company/Mammal.java
@@ -0,0 +1,39 @@
+package com.company;
+
+public class Mammal {
+    //k k hola ta ??
+    // This is how i write the single line comment
+
+
+    /*
+     * This is how I can write the multiple line comments
+     *
+     *
+     *
+     *
+     *
+     * */
+
+
+
+
+    public Mammal(String scientificName) {
+        System.out.println("initilialise this object ");
+        System.out.println("hello git");
+        Fruit fruit = new Fruit();
+        //fruit.
+       // fruit.set
+
+
+
+    }
+
+    String scientificName;
+    boolean hasFur;
+    boolean canGiveBirth;
+
+
+    public Mammal() {
+
+    }
+}
